8de4d1ce76ee58254b330c3d6199fa117c442738
aita <792803+aita@users.noreply.github.com>
2018-10-16 21:56:28 +0900

    メソッドである必要のないinsert_letをトップレベルに移動

--- mincaml/knorm.py
+++ mincaml/knorm.py
@@ -23,6 +23,22 @@ def IR_factory(op, *options, flat=True):
 FunDef = namedtuple("FunDef", "typ name args body")
 
 
+def insert_let(env, factory, exps):
+    letenv, args = [], []
+    for (e, t) in exps:
+        if isinstance(e, syntax.Var):
+            args.append(e.name)
+        else:
+            x = gen_tmp_id(t)
+            letenv.append(((x, t), e))
+            args.append(x)
+
+    e = factory(*args)
+    for (x, t1), e1 in reversed(letenv):
+        e = IR("Let", (x, t1), e1, e)
+    return e
+
+
 class Visitor:
     def __init__(self, extenv):
         self.extenv = extenv
@@ -58,9 +74,9 @@ class Visitor:
         elif e.op == "-.":
             op, typ = "FNeg", types.Float
         else:
-            raise ValueError(f"unknown operator '{e.op}'")
+            raise ValueError(f"unknown unary operator '{e.op}'")
 
-        return self.insert_let(env, IR_factory(op), [self.visit(env, e.arg)]), typ
+        return insert_let(env, IR_factory(op), [self.visit(env, e.arg)]), typ
 
     def visit_BinaryExp(self, env, e):
         if e.op in ("=", "<="):
@@ -84,10 +100,10 @@ class Visitor:
         elif e.op == "/.":
             op, t = "FDiv", types.Float
         else:
-            raise ValueError(f"unknown operator '{e.op}'")
+            raise ValueError(f"unknown binary operator '{e.op}'")
 
         return (
-            self.insert_let(
+            insert_let(
                 env, IR_factory(op), [self.visit(env, e.left), self.visit(env, e.right)]
             ),
             t,
@@ -104,12 +120,12 @@ class Visitor:
             elif e.cond.op == "=":
                 op = "IfEq"
             else:
-                raise ValueError(f"unknown operator: {e.cond.op}")
+                raise ValueError(f"unknown comparison operator: {e.cond.op}")
 
             e3, t3 = self.visit(env, e.then)
             e4, t4 = self.visit(env, e.else_)
             return (
-                self.insert_let(
+                insert_let(
                     env,
                     lambda x, y: IR(op, x, y, e3, e4),
                     [self.visit(env, e.cond.left), self.visit(env, e.cond.right)],
@@ -143,17 +159,14 @@ class Visitor:
     def visit_Tuple(self, env, e):
         xs = [self.visit(env, e) for e in e.elems]
         return (
-            self.insert_let(env, IR_factory("Tuple", flat=False), xs),
+            insert_let(env, IR_factory("Tuple", flat=False), xs),
             types.Tuple([t for _, t in xs]),
         )
 
     def visit_LetTuple(self, env, e):
         e1, t1 = self.visit(env, e.bound)
         e2, t2 = self.visit(env.update(dict(e.pat)), e.body)
-        return (
-            self.insert_let(env, lambda x: ("LetTuple", e.pat, x, e2), [(e1, t1)]),
-            t2,
-        )
+        return (insert_let(env, lambda x: ("LetTuple", e.pat, x, e2), [(e1, t1)]), t2)
 
     def visit_Var(self, env, e):
         if e.name in env:
@@ -169,7 +182,7 @@ class Visitor:
         e2, t2 = self.visit(env, e.init)
         ctor = "create_float_array" if types.is_float(t2) else "create_array"
         return (
-            self.insert_let(
+            insert_let(
                 env, IR_factory("ExtFunApp", ctor, flat=False), [(e1, t1), (e2, t2)]
             ),
             types.Array(t2),
@@ -179,7 +192,7 @@ class Visitor:
         if isinstance(e.fun, syntax.Var) and e.fun.name not in env:
             if e.fun.name in self.extenv and types.is_fun(self.extenv[e.fun.name]):
                 return (
-                    self.insert_let(
+                    insert_let(
                         env,
                         IR_factory("ExtFunApp", e.fun.name, flat=False),
                         [self.visit(env, arg) for arg in e.args],
@@ -192,7 +205,7 @@ class Visitor:
         e1, t1 = self.visit(env, e.fun)
         if types.is_fun(t1):
             return (
-                self.insert_let(
+                insert_let(
                     env,
                     lambda x, *ys: ("App", x, ys),
                     [(e1, t1)] + [self.visit(env, arg) for arg in e.args],
@@ -200,22 +213,20 @@ class Visitor:
                 t1.ret,
             )
         else:
-            raise ValueError(f"unknown function type: {t1}")
+            raise ValueError(f"cannot apply {t1}")
 
     def visit_Get(self, env, e):
         e1, t1 = self.visit(env, e.array)
         if not types.is_array(t1):
             raise ValueError(f"cannot get from {t1}")
         return (
-            self.insert_let(
-                env, IR_factory("Get"), [(e1, t1), self.visit(env, e.index)]
-            ),
+            insert_let(env, IR_factory("Get"), [(e1, t1), self.visit(env, e.index)]),
             t1.elem,
         )
 
     def visit_Put(self, env, e):
         return (
-            self.insert_let(
+            insert_let(
                 env,
                 IR_factory("Put"),
                 [
@@ -226,21 +237,6 @@ class Visitor:
             ),
             types.Unit,
         )
-
-    def insert_let(self, env, factory, exps):
-        letenv, args = [], []
-        for (e, t) in exps:
-            if isinstance(e, syntax.Var):
-                args.append(e.name)
-            else:
-                x = gen_tmp_id(t)
-                letenv.append(((x, t), e))
-                args.append(x)
-
-        e = factory(*args)
-        for (x, t1), e1 in reversed(letenv):
-            e = IR("Let", (x, t1), e1, e)
-        return e
 
 
 def normalize(e, extenv):
